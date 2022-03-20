bc21df8e69f2668c332340814d70fdfc064004e9
RzLModz <97414556+RzLModz@users.noreply.github.com>
2022-03-20 21:49:55 +0700

    Add files via upload

--- setting/rules.js
+++ setting/rules.js
@@ -6,13 +6,15 @@ const rules = (prefix, botname) => {
 return `*_RULES BOT_*
 
 1. Jangan spam bot. 
-Sanksi: *WARN/SOFT BLOCK*
+Sanksi: *GUA BLOK AJG*
 
 2. Jangan telepon bot.
-Sanksi: *SOFT BLOCK*
+Sanksi: *GUA BLOK TAPI KLO CEWE GPP*
 
-3. Jangan mengeksploitasi bot.
-Sanksi: *PERMANENT BLOCK*
+3. Jangan memnghina/menghujat bot.
+Sanksi: *GUA BLOK NOMER LU + WAR SANTED*
+
+_BOT NYOLONG SC ORG/AUTO RES/WIBU AE BANGGA JANGAN SOK PRO DEK !_
 
 Jika sudah dipahami rules-nya, silakan ketik *${prefix}menu* untuk memulai!
 
